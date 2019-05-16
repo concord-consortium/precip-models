dbeb051551aeb363715f2de090e1c3dc2a72ef85
Noah Paessel <knowuh@gmail.com>
2019-05-16 15:07:13 -0400

    Add New Netlogo files, new html, new css.
    Update readme:
    
    [#166069981]
    
    https://www.pivotaltracker.com/story/show/166069981

--- README.md
+++ README.md
@@ -7,3 +7,25 @@ To 'deploy' these models, copy files to the S3 bucket named:
 You will probably want to invalidate the cloudfront cache manually, because the min TTL is 24 hours.
 
 The `add-css.sh` file causes certain NetLogo chrome to be hidden.
+
+The deployed models can then be found on S3 webiste, at this URLS:
+
+Alaska:
+https://models-resources.concord.org/precip-models/lesson-4-ak.html
+https://models-resources.concord.org/precip-models/lesson-5-ak.html
+https://models-resources.concord.org/precip-models/precip-rule-ak.html
+
+Alaska V2:
+https://models-resources.concord.org/precip-models/lesson-4-ak-v2.html
+https://models-resources.concord.org/precip-models/precip-rule-ak-v2.html
+
+
+New England:
+https://models-resources.concord.org/precip-models/lesson-4-ne.html
+https://models-resources.concord.org/precip-models/lesson-5-ne.html
+https://models-resources.concord.org/precip-models/precip-rule-ne.html
+
+New England V2:
+https://models-resources.concord.org/precip-models/lesson-4-ne-v2.html
+https://models-resources.concord.org/precip-models/precip-rule-ne-v2.html
+
--- add-css.sh
+++ add-css.sh
@@ -2,14 +2,32 @@ cp ./lesson-4-ak-in.html ./lesson-4-ak.html
 cp ./lesson-5-ak-in.html ./lesson-5-ak.html
 cp ./precip-rule-ak-in.html ./precip-rule-ak.html
 
+cp ./lesson-4-ak-v2-in.html ./lesson-4-ak-v2.html
+# cp ./lesson-5-ak-v2-in.html ./lesson-5-ak-v2.html
+cp ./precip-rule-ak-v2-in.html ./precip-rule-ak-v2.html
+
 cp ./lesson-4-ne-in.html ./lesson-4-ne.html
 cp ./lesson-5-ne-in.html ./lesson-5-ne.html
 cp ./precip-rule-ne-in.html ./precip-rule-ne.html
+
+cp ./lesson-4-ne-v2-in.html ./lesson-4-ne-v2.html
+# cp ./lesson-5-ne-v2-in.html ./lesson-5-ne-v2.html
+cp ./precip-rule-ne-v2-in.html ./precip-rule-ne-v2.html
+
 
 cat ./extra-css.html >> ./lesson-4-ak.html
 cat ./extra-css.html >> ./lesson-5-ak.html
 cat ./extra-css.html >> ./precip-rule-ak.html
 
+cat ./extra-css.html >> ./lesson-4-ak-v2.html
+# cat ./extra-css.html >> ./lesson-5-ak-v2.html
+cat ./extra-css.html >> ./precip-rule-ak-v2.html
+
+
 cat ./extra-css.html >> ./lesson-4-ne.html
 cat ./extra-css.html >> ./lesson-5-ne.html
 cat ./extra-css.html >> ./precip-rule-ne.html
+
+cat ./extra-css.html >> ./lesson-4-ne-v2.html
+# cat ./extra-css.html >> ./lesson-5-ne-v2.html
+cat ./extra-css.html >> ./precip-rule-ne-v2.html
